94fd24b29bf1d66b1cf9fcb4999582316ffb1417
Jojo Diloy <yanchai@1bit.com.ph>
2024-06-18 14:59:57 +0800

    fixed ci/cd

--- deploy_development.sh
+++ deploy_development.sh
@@ -1,8 +1,5 @@
 # Change to the project directory
 
-# Turn on maintenance mode
-pm2 stop microservice-notification
-
 git checkout $SITE_BRANCH
 # Pull the latest changes from the git repository
 git reset --hard
@@ -12,9 +9,6 @@ git pull
 
 # Install dependencies
 npm install
-
-# Restart pm2 instance
-pm2 restart microservice-notification
 
 COMMIT_AUTHOR=$(git log -1 --pretty=%an | cat)
 COMMIT_MESSAGE=$(git log -1 --pretty=%B | cat | tr -d '"' | sed '/^[[:space:]]*$/d')
