30783eb1e813cf1287da9a9570b9b60784edb49c
Jojo Diloy <yanchai@1bit.com.ph>
2024-06-18 14:47:37 +0800

    added ci/cd to butterfly dev server

---
+++ deploy_development.sh
@@ -0,0 +1,20 @@
+# Change to the project directory
+
+# Turn on maintenance mode
+php artisan down
+
+git checkout $SITE_BRANCH
+# Pull the latest changes from the git repository
+git reset --hard
+git clean -df
+git pull
+
+
+# Install dependencies
+npm install
+
+COMMIT_AUTHOR=$(git log -1 --pretty=%an | cat)
+COMMIT_MESSAGE=$(git log -1 --pretty=%B | cat | tr -d '"' | sed '/^[[:space:]]*$/d')
+
+
+./notif.sh "butterfly-api-BE deployment on Development server completed [${COMMIT_AUTHOR}]: ${COMMIT_MESSAGE}"
